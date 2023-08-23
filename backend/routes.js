eda798f9b6459da5539117ad99f5ae793ef346a3
BigSiimp <lukas.f.bade@gmail.com>
2023-08-23 22:37:21 +0200

    Update routes.js

--- backend/routes.js
+++ backend/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./db'); // Passe den Pfad an
+const db = require('./DB'); 
 
 router.post('/insert', (req, res) => {
   const newData = {
