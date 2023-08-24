368fc9860f4147d3d59087eb08bfe5c8263f6b97
BigSiimp <lukas.f.bade@gmail.com>
2023-08-24 12:27:55 +0200

    Update server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -2,9 +2,6 @@
 const express = require('express');
 const dbConnection = require('./db.js');
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const errorHandler = require('_middleware/error-handler');
 const PORT = 3001;
 
 app.get('/', (req, res) => {
