52e86ba94bc3329f9e55e636ef2e36371918eda7
BigSiimp <lukas.f.bade@gmail.com>
2023-08-25 12:25:41 +0200

    Update App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import './App.css'; Importieren Sie Ihre eigenen Styles oder verwenden Sie Bootstrap-Klassen
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Meine React-Anwendung</h1>
+      </header>
+      <main>
+        {/* Hier können Sie Ihre Komponenten einfügen */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
