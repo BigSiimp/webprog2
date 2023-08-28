d0effba637d8e32b833250ecd390bdb7ceb584f0
BigSiimp <lukas.f.bade@gmail.com>
2023-08-28 16:48:05 +0200

    gg

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'; // Import der Hauptkomponente
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importieren des Bootstrap CSS
 
 ReactDOM.render(
   <React.StrictMode>
